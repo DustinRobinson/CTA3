648cb092d838062b2c00402645929f24c190701d
Dustin Robinson <marshall.robinson@protonmail.com>
2024-03-09 20:17:57 -0700

    fixed comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  * main.cpp
  *
  *  Created on: Mar 9, 2024
- *      Author: marsh
+ *  Author: marsh
  */
 
 #include <iostream>
@@ -13,13 +13,16 @@ using namespace std;
 void setIntFromInput(int *p)
 	{
 
+		//requests number from user
 		std::cout << "Please, enter a number.";
 		std::cin >> *p;
 
 			while (std::cin.fail())
 			{
+				//cleares buffer/ignores rest of input stream
 				std::cin.clear();
 				std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+				//requests number from user, and prompts with error message
 				std::cout<<"Error! Try again!" << endl;
 				std::cout <<"Please, enter a number." << endl;
 				std::cin >> *p;
@@ -50,6 +53,7 @@ int main() {
 	cout << "The second number is " << num2 << ", and its value is held at address: " << p2 << endl;
 	cout << "The third number is " << num3 << ", and its value is held at address: " <<  p3 << endl;
 
+	//deletes and sets pointer to null pointer to manage memory before exit
 	delete p1;
 	p1 = nullptr;
 	delete p2;
